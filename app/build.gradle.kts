fd4d289729eb29d8ef60dff7cb49e927b38115a6
Asad Mehmood <wicaodian@gmail.com>
2024-03-07 17:34:08 +0500

    initial release

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.cardslider"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.cardslider"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
